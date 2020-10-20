8267506af79063c40f23912d996957aa5eea2ad3
Kevin Poirier <kevin.poirier@ozytis.fr>
2020-10-20 09:49:12 +0200

    Fichier manquant

---
+++ Entities/Enums/Confident.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Enums
+{
+    public enum Confident
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2
+    }
+}
